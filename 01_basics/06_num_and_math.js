1a337b17bdf927428973ba24236fd403868b25dc
gauravasodariya <gauravasodariya44@gmail.com>
2024-12-08 15:10:23 +0000

    num_and_math

---
+++ 01_basics/06_num_and_math.js
@@ -0,0 +1,34 @@
+// const score=400
+// console.log(score)
+
+// const balance=new Number(100.55)
+// console.log(balance)
+
+// console.log(balance.toString().length)
+// console.log(balance.toFixed(5))
+
+// const otherNumber=23.589
+// console.log(otherNumber.toPrecision(4));
+
+// const hundread=1000000
+// console.log(hundread.toLocaleString("en-IN"));
+// console.log(Number.MAX_VALUE);
+
+//--------------------Maths---------------------------
+// console.log(Math);
+// console.log(Math.abs(-4)); //convert negative value in positive
+// console.log(Math.round(4.6));
+// console.log(Math.ceil(4.1));
+// console.log(Math.floor(4.9));
+// console.log(Math.pow(2,5));
+// console.log((Math.min(23,96,45,87,12)));
+// console.log((Math.max(23,96,45,87,12)));
+// console.log(Math.random()); //always give value between 0 and 1 and also give 1 and 0
+// console.log(Math.random()*10+1); //we add +1 to avoid zero value
+
+const min=10
+const max=20
+console.log(Math.floor(Math.random()*(max-min)+1))+min
+
+
+
