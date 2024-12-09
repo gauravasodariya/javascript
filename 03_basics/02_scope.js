2ae5b01b10e8cb8693dcf1f885de355cec9674e7
gauravasodariya <gauravasodariya44@gmail.com>
2024-12-09 12:09:39 +0000

    02_scope

--- 03_basics/02_scope.js
+++ 03_basics/02_scope.js
@@ -9,15 +9,55 @@
 
 //{} : scope
 
-if(true)
+// if(true)
+// {
+//     let a=10
+//     const b=20
+//     var c=30
+// }
+// //Error
+// console.log(a);
+// console.log(b);
+// console.log(c); //No Error, due to var keyword and it has global scope and const and let has local or block scope
+
+
+//nested scope
+// function one()
+// {
+//     const username="Raj"
+//     function two()
+//     {
+//         const website="youtube"
+//         console.log(username);
+//     }
+//    // console.log(website);
+//     two()
+// }
+// one()
+
+// if(true)
+// {
+//     const username="Raj"
+//     if(username=="Raj")
+//     {
+//         const website=" youtube"
+//         console.log(username+website);
+//     }
+//    // console.log(website); Error
+// }
+//console.log(username); Error
+
+
+//addOne(5) before function initialization ,No Error
+function addOne(num)
 {
-    let a=10
-    const b=20
-    var c=30
+    return num+1
 }
-//Error
-console.log(a);
-console.log(b);
-console.log(c); //No Error, due to var keyword and it has global scope and const and let has local or block scope
+addOne(5)
 
-
+//addTwo(5)  Give Error,due to before function initialization, we store returned value in addTwo variable
+const addTwo=function(num)
+{
+    return num+2
+}
+//addTwo(5)
