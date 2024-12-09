2b78e36ca1180c0b40fa60c98e05f1369fe5689b
gauravasodariya <gauravasodariya44@gmail.com>
2024-12-09 09:59:01 +0000

    2_objects

---
+++ 02_basics/04_objects.js
@@ -0,0 +1,31 @@
+const jsUser=
+{
+    name: "Raj",
+    "full name" : "ravi patel",
+    age : 18,
+    location : "Amreli",
+    email : "ravi@gmail.com",
+    isloggedIn : false,
+    lastLogin :['monday','saturday']
+}
+const course={
+    coursename : "javascript",
+    price : 3999,
+    instructor : "prakash"
+}
+// de-structure of objects
+//1. const {instructor}=course
+//2. const {instructor : inst} = course //
+//console.log(inst);
+
+//3.
+// const navbar= ({company}) =>{
+// }
+// navbar(company="raj")
+
+//json
+// {
+//     name : "raj",
+//     course : "Javascipt",
+//     price : 2999
+// }
