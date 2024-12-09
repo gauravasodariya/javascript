182a98c11cd0026ab5626ef08345d20e4e1a866a
gauravasodariya <gauravasodariya44@gmail.com>
2024-12-09 05:57:31 +0000

    01_array

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,37 @@
+const myArr=[1,2,3,4,5]
+//javascript array are resizeable and can contain a mix of different datatypes
+//javascript array are not associative arrays and array element cannot be accessed using rbitary string as index
+//console.log(myArr[0]);
+//another method to declare array
+const myHeros=['a','b','c','d','e']
+const myArr2=new Array(1,2,3,4)
+myArr2.push(5) //insert value in the last position
+//console.log(myArr2);
+// myArr2.pop()
+// console.log(myArr2);
+// myArr2.unshift(10) //insert value in the first position
+// console.log(myArr2);
+// myArr2.shift() //remove element from first position
+// console.log(myArr2);
+
+// console.log(myArr2.includes(5)); //return true or false
+// console.log(myArr2.indexOf(30)); //if value does not exist then return -1
+
+// const newArr=myArr2.join()
+// console.log(myArr2);
+// console.log(typeof newArr);
+
+ console.log("A",myArr);
+ const myn1=myArr.slice(1,3)
+ console.log(myn1);
+// console.log("B",myArr);
+
+const myn2=myArr.splice(1,3)
+console.log(myArr);
+console.log(myn2);
+/*slice(): The slice() method is used to extract a portion of an array and returns a new array containing the extracted elements. splice(): The splice() method is used to change the contents of an array by removing or replacing existing elements and/or adding new elements in place*/
+
+
+
+
+
