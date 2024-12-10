4dc1dcd233f8f4c66867bc73dd04214d6deee864
gauravasodariya <gauravasodariya44@gmail.com>
2024-12-10 13:30:15 +0000

    for_loop

---
+++ 05_iterations/01_for_loop.js
@@ -0,0 +1,29 @@
+// for loop
+// for (let i = 0; i < 10; i++) {
+//     console.log(i);
+// }
+
+
+// //print mutiplication table
+// for(let i=1;i<=10;i++)
+// {
+//     for(let j=1;j<=10;j++)
+//     {
+//         console.log(i + " * " +j + " = " + i*j);
+//     }
+// }
+
+//break and continue
+for (let i = 0; i < 10; i++)
+{
+    if(i==5)
+    {
+        continue
+    }
+    if(i==8)
+    {
+        break
+    }
+    console.log(i);
+    
+}
