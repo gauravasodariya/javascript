e352ed8d3d7bc2007877675e08ca44b50be286f4
gauravasodariya <gauravasodariya44@gmail.com>
2024-12-10 17:03:58 +0000

    while_loop

---
+++ 05_iterations/02_while_loop.js
@@ -0,0 +1,25 @@
+//while loop
+
+// let i=0
+// while(i<=10)
+// {
+//     console.log(i);
+//     i+=2 // i=i+2
+// }
+
+
+// let myArray=['a','b','c','d']
+// let arr=0
+// while(arr<myArray.length)
+// {
+//     console.log(`value is ${arr} is : ${myArray[arr]}`);
+//     arr++
+// }
+
+//do-while loop
+let score=11
+do
+{
+    console.log(`Score is ${score}`);
+    score++
+}   while(score<=10)
