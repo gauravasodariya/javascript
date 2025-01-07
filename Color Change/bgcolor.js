1df5718ca91e29ecb47124647a8687df0a99cdb9
gauravasodariya <gauravasodariya44@gmail.com>
2025-01-07 08:59:14 +0000

    chnagecolor

--- Color Change/bgcolor.js
+++ Color Change/bgcolor.js
@@ -4,8 +4,24 @@ const randomColor=function(){
     let color="#";
     for(let i=0;i<6;i++)
     {
-        color=color+Math.random()*16;
+        color=color+hex[Math.floor(Math.random()*16)];
     }
+    return color;
+};
+let stopps;
+const demo=function(){
+    stopps=setInterval(startToChangeColor,1000);
 }
+const startToChangeColor=function()
+{
+    document.body.style.backgroundColor=randomColor();
+};
+const stopToChangeColor=function()
+{
+    clearInterval(stopps);
+    stopps=null;
+};
+document.querySelector('#start').addEventListener('click',demo);
+document.querySelector('#stop').addEventListener('click',stopToChangeColor);
 
 
